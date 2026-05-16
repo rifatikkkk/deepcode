f2621649df950248dd5d3eb45e1909aafb1a60c7
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-05-17 00:38:50 +0300

    feat: add routing for card

---
+++ client/src/pages/Card/ui/CardPage.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "@consta/uikit/Layout";
+
+const CardPage = () => {
+  return (
+    <Layout direction="column" style={{ gap: "20px", padding: "40px" }}>
+      <div>card page</div>
+    </Layout>
+  );
+};
+
+export default CardPage;
--- client/src/shared/ui/Table/Table.tsx
+++ client/src/shared/ui/Table/Table.tsx
@@ -1,16 +1,30 @@
 import { Table, type TableColumn } from "@consta/table/Table";
+import { useNavigate } from "react-router";
 
-type CustomTableProps<T> = {
+interface HasId {
+  id: number;
+}
+
+type CustomTableProps<T extends HasId> = {
   tableKey: "users-table" | "posts-table" | undefined;
   tableRows: T[];
   tableColumns: TableColumn<T>[];
 };
 
-export const CustomTable = <T,>({
+export const CustomTable = <T extends HasId>({
   tableRows,
   tableColumns,
   tableKey,
 }: CustomTableProps<T>) => {
+  const navigate = useNavigate();
+
+  const navigateToCard = (id: number) => {
+    if (tableKey) {
+      navigate(
+        tableKey === "users-table" ? `/card/user/${id}` : `/card/post/${id}`,
+      );
+    }
+  };
   return (
     <Table
       key={tableKey}
@@ -18,6 +32,7 @@ export const CustomTable = <T,>({
       columns={tableColumns}
       stickyHeader
       style={{ maxHeight: 500, maxWidth: 1000 }}
+      onRowClick={({ id }) => navigateToCard(id)}
     />
   );
 };
