5ef67e012aa86442104bc8eb90194eea64e9e6fe
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-05-17 20:28:18 +0300

    fix: count page in pagination

--- client/src/shared/ui/Pagination/Pagination.tsx
+++ client/src/shared/ui/Pagination/Pagination.tsx
@@ -19,7 +19,7 @@ export const CustomPagination: FC<CustomPaginationProps> = ({
       value={value}
       onChange={onChange}
       arrows={[{ label: "Предыдущая" }, { label: "Следующая" }]}
-      visibleCount={1}
+      visibleCount={5}
     />
   );
 };
